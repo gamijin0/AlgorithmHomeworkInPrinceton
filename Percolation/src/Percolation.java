7f774bd6293f3b64ec189557011985dd7d7a4a86
chaos <497386856@qq.com>
2017-03-15 22:22:49 +0800

    recorrect the required index of para; recorrect the isFull() func;

--- Percolation/src/Percolation.java
+++ Percolation/src/Percolation.java
@@ -29,37 +29,50 @@ public class Percolation {
         }
 
     }     // create n-by-n grid, with all sites blocked
-    public void open(int row, int col) throws IndexOutOfBoundsException{
+    public void open(int row, int col) {
+        row = row -1;
+        col = col -1;
         if( row >= this.grid.length || col >= this.grid.length || row < 0 ||col < 0){
             throw new IndexOutOfBoundsException();
         }
         if(!this.isOpen(row,col)){
-            if(row!=0 && this.grid[row-1][col]){
-                this.weightedQuickUnionUF.union(getIndex(row,col),getIndex(row-1,col));
+            if(row>0 ){
+                if(this.isOpen(row-1,col))
+                    this.weightedQuickUnionUF.union(getIndex(row,col),getIndex(row-1,col));
             }
-            if(col!=0 && this.grid[row][col-1]){
-                this.weightedQuickUnionUF.union(getIndex(row,col),getIndex(row,col-1));
+            if(col>0 ){
+                if(this.isOpen(row,col-1))
+                    this.weightedQuickUnionUF.union(getIndex(row,col),getIndex(row,col-1));
             }
-            if(row!=this.grid.length-1 && this.grid[row+1][col]){
-                this.weightedQuickUnionUF.union(getIndex(row,col),getIndex(row+1,col));
+            if(row<this.grid.length-1){
+                if(this.isOpen(row+1,col))
+                    this.weightedQuickUnionUF.union(getIndex(row,col),getIndex(row+1,col));
             }
-            if(col!=this.grid.length-1 && this.grid[row][col+1]){
-                this.weightedQuickUnionUF.union(getIndex(row,col),getIndex(row,col+1));
+            if(col<this.grid.length-1 ){
+                if(this.isOpen(row,col+1))
+                    this.weightedQuickUnionUF.union(getIndex(row,col),getIndex(row,col+1));
             }
             this.grid[row][col]=true;
         }
     }    // open site (row, col) if it is not open already
-    public boolean isOpen(int row, int col) throws IndexOutOfBoundsException{
-        if( row >= this.grid.length || col >= this.grid.length){
+    public boolean isOpen(int row, int col) {
+        row = row -1;
+        col = col -1;
+        if( row >= this.grid.length || col >= this.grid.length || row < 0 ||col < 0){
             throw new IndexOutOfBoundsException();
         }
         return this.grid[row][col];
     }  // is site (row, col) open?
-    public boolean isFull(int row, int col) throws IndexOutOfBoundsException{
-        if( row >= this.grid.length || col >= this.grid.length){
+    public boolean isFull(int row, int col) {
+        row = row -1;
+        col = col -1;
+        if( row >= this.grid.length || col >= this.grid.length || row < 0 ||col < 0){
             throw new IndexOutOfBoundsException();
         }
-        return this.weightedQuickUnionUF.connected(0,this.getIndex(row,col));
+        if(this.isOpen(row,col))
+            return this.weightedQuickUnionUF.connected(0,this.getIndex(row,col));
+        else
+            return false;
     }  // is site (row, col) full?
     public int numberOfOpenSites(){
         int num=0;
