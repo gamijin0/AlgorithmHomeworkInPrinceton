1ad5a87aa2229aee065227d22c95c51e1eb92b8e
chaos <497386856@qq.com>
2017-04-13 14:23:04 +0800

    add a Brute

---
+++ PA3/src/BruteCollinearPoints.java
@@ -0,0 +1,10 @@
+public class BruteCollinearPoints {
+    public BruteCollinearPoints(Point[] points) {
+        if (points == null)
+            throw new NullPointerException();
+
+    }
+    public
+    int numberOfSegments()
+    public LineSegment[] segments()
+}
